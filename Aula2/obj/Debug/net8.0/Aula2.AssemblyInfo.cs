8af6764e224ba444a8d57555d897a81776832c01
Gabriel Gravena Barros <132004805+oppsggbarros@users.noreply.github.com>
2024-11-01 16:10:56 -0400

    Exercicio matriz/vetores

--- Aula2/obj/Debug/net8.0/Aula2.AssemblyInfo.cs
+++ Aula2/obj/Debug/net8.0/Aula2.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Aula2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b3c1daffceebfc99e8eefeca94418a6299ed4d2f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a4ef7c1e0cd002e39e16cd165a4697323ab283b3")]
 [assembly: System.Reflection.AssemblyProductAttribute("Aula2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Aula2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- Aula4/Vetores.cs
+++ Aula4/Vetores.cs
@@ -1,38 +1,189 @@
-﻿using System;
-using System.Linq;
-using System.Collections.Generic;
-using System.Security.Cryptography;
-namespace exercicio
-{
-    internal class Produtos
-    {
-        static void Main (String[] args)
-        {
-            // int [] vetor1 = new int[4];
-            // int [] vetor2 = {1, 2, 3, 4, 5};
-            // int [] vetor3 = new int[] {1, 2, 3, 4, 5};
-            // int [] vetor4 = {};
+﻿// using System;
+// using System.Linq;
+// using System.Collections.Generic;
+// using System.Security.Cryptography;
+// namespace exercicio
+// {
+//     internal class Produtos
+//     {
+//         static void Main (String[] args)
+//         {
+//             // int [] vetor1 = new int[4];
+//             // int [] vetor2 = {1, 2, 3, 4, 5};
+//             // int [] vetor3 = new int[] {1, 2, 3, 4, 5};
+//             // int [] vetor4 = {};
 
-        //     int[] x = new int[10];
-        //     int i;
-        //     Console.WriteLine("Digite 10 valores inteiros");
-        //     for (i = 0; i < 10; i++)
-        //         x[i] = int.Parse(Console.ReadLine());
-        //         Console.WriteLine("Ordem inversa");
-        //     for (i = 9; i >= 0; i--)
-        //         Console.WriteLine(x[i]);
-        //         Console.ReadKey();
-            Random r = new Random();
-            int[] vetor = new int[10] { r.Next(10), r.Next(10), r.Next(10), r.Next(10), r.Next(10), r.Next(10), r.Next(10), r.Next(10), r.Next(10), r.Next(10) };
-            Console.WriteLine(string.Join(", ", vetor));
-            Console.WriteLine(vetor);
+//         //Exercicio 1
+//         //     int[] x = new int[10];
+//         //     int i;
+//         //     Console.WriteLine("Digite 10 valores inteiros");
+//         //     for (i = 0; i < 10; i++)
+//         //         x[i] = Convert.ToInt32(Console.ReadLine());
+//         //         Console.WriteLine("Ordem inversa");
+//         //     for (i = 9; i >= 0; i--)
+//         //         Console.WriteLine(x[i]);
+//         //         Console.ReadKey();
+//             //Exercicio 2
 
-            int maior = vetor.Max();
-            int menor = vetor.Min();
+//             // Random r = new Random();
+//             // int[] vetor = new int[10] { r.Next(10), r.Next(10), r.Next(10), r.Next(10), r.Next(10), r.Next(10), r.Next(10), r.Next(10), r.Next(10), r.Next(10) };
+//             // Console.WriteLine(string.Join(", ", vetor));
+//             // Console.WriteLine(vetor);
 
-            Console.WriteLine("Maior valor: " + maior);
-            Console.WriteLine("Menor valor: " + menor);
+//             // int maior = vetor.Max();
+//             // int menor = vetor.Min();
 
-        }
-    }
-}
+//             // Console.WriteLine("Maior valor: " + maior);
+//             // Console.WriteLine("Menor valor: " + menor);
+        
+//         // Exercicio 3
+//             // int[,] matriz = new int[4, 4];
+//             // Console.WriteLine("Digite os elementos da matriz 4x4:");
+//             // for (int i = 0; i < 4; i++)
+//             // {
+//             //     for (int j = 0; j < 4; j++)
+//             //     {
+//             //         Console.Write($"Elemento [{i + 1},{j + 1}]: ");
+//             //         matriz[i, j] = Convert.ToInt32(Console.ReadLine());
+//             //     }
+//             // }
+
+            
+//             // int menor = matriz[0, 0];
+//             // int maior = matriz[0, 0];
+
+            
+//             // for (int i = 0; i < 4; i++)
+//             // {
+//             //     for (int j = 0; j < 4; j++)
+//             //     {
+//             //         if (matriz[i, j] < menor)
+//             //             menor = matriz[i, j];
+                    
+//             //         if (matriz[i, j] > maior)
+//             //             maior = matriz[i, j];
+//             //     }
+//             // }
+
+            
+//             // int soma = maior + menor;
+
+            
+//             // Console.WriteLine($"O menor elemento é: {menor}");
+//             // Console.WriteLine($"O maior elemento é: {maior}");
+//             // Console.WriteLine($"A soma do maior com o menor elemento é: {soma}");
+
+//             // Exercicio 4
+
+//             // int[,] matriz = new int[4, 4];
+        
+//             // Console.WriteLine("Digite os elementos da matriz 4x4:");
+//             // for (int i = 0; i < 4; i++)
+//             // {
+//             //     for (int j = 0; j < 4; j++)
+//             //     {
+//             //         Console.Write($"Elemento [{i + 1},{j + 1}]: ");
+//             //         matriz[i, j] = Convert.ToInt32(Console.ReadLine());
+//             //     }
+//             // }
+
+            
+//             // int somaDiagonal = 0;
+//             // for (int i = 0; i < 4; i++)
+//             // {
+//             //     somaDiagonal += matriz[i, i];
+//             // }
+
+            
+//             // Console.WriteLine($"A soma dos elementos da diagonal principal é: {somaDiagonal}");
+
+//             //Exercicio 5
+
+//             // int[,] matriz1 = new int[4, 4];
+//             // int[,] matriz2 = new int[4, 4];
+//             // int[,] somaMatriz = new int[4, 4];
+
+//             // Console.WriteLine("Digite os elementos da primeira matriz 4x4:");
+//             // for (int i = 0; i < 4; i++)
+//             // {
+//             //     for (int j = 0; j < 4; j++)
+//             //     {
+//             //         Console.Write($"Elemento [{i + 1},{j + 1}]: ");
+//             //         matriz1[i, j] = Convert.ToInt32(Console.ReadLine());
+//             //     }
+//             // }
+
+//             // Console.WriteLine("Digite os elementos da segunda matriz 4x4:");
+//             // for (int i = 0; i < 4; i++)
+//             // {
+//             //     for (int j = 0; j < 4; j++)
+//             //     {
+//             //         Console.Write($"Elemento [{i + 1},{j + 1}]: ");
+//             //         matriz2[i, j] = Convert.ToInt32(Console.ReadLine());
+//             //     }
+//             // }
+
+//             // for (int i = 0; i < 4; i++)
+//             // {
+//             //     for (int j = 0; j < 4; j++)
+//             //     {
+//             //         somaMatriz[i, j] = matriz1[i, j] + matriz2[i, j];
+//             //         multiplicaMatriz[i, j] = matriz1[i, j] * matriz2[i, j];
+//             //     }
+//             // }
+
+//             // Console.WriteLine("A matriz resultante da soma é:");
+//             // for (int i = 0; i < 4; i++)
+//             // {
+//             //     for (int j = 0; j < 4; j++)
+//             //     {
+//             //         Console.Write(somaMatriz[i, j] + "\t");
+//             //     }
+//             //     Console.WriteLine();
+            
+//             //Exercicio 6
+
+//             int[,] matriz1 = new int[4, 4];
+//             int[,] matriz2 = new int[4, 4];
+//             int[,] multiMatriz = new int[4, 4];
+
+//             Console.WriteLine("Digite os elementos da primeira matriz 4x4:");
+//             for (int i = 0; i < 4; i++)
+//             {
+//                 for (int j = 0; j < 4; j++)
+//                 {
+//                     Console.Write($"Elemento [{i + 1},{j + 1}]: ");
+//                     matriz1[i, j] = Convert.ToInt32(Console.ReadLine());
+//                 }
+//             }
+
+//             Console.WriteLine("Digite os elementos da segunda matriz 4x4:");
+//             for (int i = 0; i < 4; i++)
+//             {
+//                 for (int j = 0; j < 4; j++)
+//                 {
+//                     Console.Write($"Elemento [{i + 1},{j + 1}]: ");
+//                     matriz2[i, j] = Convert.ToInt32(Console.ReadLine());
+//                 }
+//             }
+
+//             for (int i = 0; i < 4; i++)
+//             {
+//                 for (int j = 0; j < 4; j++)
+//                 {
+//                     multiMatriz[i, j] = matriz1[i, j] * matriz2[i, j];
+//                 }
+//             }
+
+//             Console.WriteLine("A matriz resultante da soma é:");
+//             for (int i = 0; i < 4; i++)
+//             {
+//                 for (int j = 0; j < 4; j++)
+//                 {
+//                     Console.Write(multiMatriz[i, j] + "\t");
+//                 }
+//                 Console.WriteLine();
+//         }
+//     }
+// }
+// }
